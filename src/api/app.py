ea66d1402ed584900b49f17c0c0938024e7f82d7
highly_loaded_ego <danil.sapog19@gmail.com>
2023-10-21 13:32:53 +0300

    fix exception handler

--- src/api/app.py
+++ src/api/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.api.routers.code_executors import code_executor_routers
+from src.api.routers.exception_handlers import setup_exception_handlers
 from src.api.routers.healthcheck import health_check_router
 
 
@@ -9,4 +10,5 @@ def create_app() -> FastAPI:
 
     app.include_router(health_check_router)
     app.include_router(code_executor_routers)
+    setup_exception_handlers(app=app)
     return app
--- src/api/routers/exception_handlers.py
+++ src/api/routers/exception_handlers.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse
 
 from src.common.exceptions import UnsupportedLanguage
+from src.common.schemas.errors import ErrorMessage
 
 
 def setup_exception_handlers(app: FastAPI) -> None:
@@ -10,14 +11,35 @@ def setup_exception_handlers(app: FastAPI) -> None:
 
 
 async def unsupported_language(request: Request, err: UnsupportedLanguage) -> ORJSONResponse:
-    return ORJSONResponse(
-        "Unsupported language!",
-        status_code=status.HTTP_404_NOT_FOUND
+    return await handle_app_error(
+        status_code=status.HTTP_404_NOT_FOUND,
+        message="Unsupported language!",
+        err=err,
+        request=request
     )
 
 
 async def unsupported_handler(request: Request, err: Exception) -> ORJSONResponse:
-    return ORJSONResponse(
-        '\n'.join(err.args),
+    return await handle_app_error(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        err_args=err.args,
+        err=err,
+        request=request
+    )
+
+
+async def handle_app_error(
+    *,
+    request: Request,
+    status_code: int,
+    err: Exception,
+    message: str | None = None,
+    err_args: tuple | None = None
+) -> ORJSONResponse:
+    if message:
+        message = message
+    else:
+        message = "\n".join(err_args)
+    return ORJSONResponse(
+        ErrorMessage(message=message), status_code=status_code
     )
